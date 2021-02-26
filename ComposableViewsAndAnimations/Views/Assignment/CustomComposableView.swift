1bc98cb7eabe98455dd0bcf680172c65628bd394
Russell Gordon <rgordon@lcs.on.ca>
2021-02-26 15:34:19 -0500

    Got the initial 12 squares rotated around the centre of the view.

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct CustomComposableView: View {
+    
+    // Initial angles of rotation for the squares
+    let initialAngles: [Double] = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            
+            ForEach(initialAngles, id: \.self) { angle in
+                
+                Rectangle()
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(Color(hue: angle/360.0, saturation: 0.8, brightness: 0.9))
+                    .rotationEffect(.degrees(angle), anchor: .topLeading)
+                    .offset(x: 50, y: 50)
+
+            }
+            
+        }
+        
     }
 }
 
